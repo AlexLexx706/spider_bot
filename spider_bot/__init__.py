66f20892b05837bf6de197045c8adb84b1fde65d
alexlexx1 <alexlexx1@gmail.com>
2018-07-24 03:23:39 +0300

    'add'

--- spider_bot/__init__.py
+++ spider_bot/__init__.py
@@ -8,10 +8,30 @@ from engine_3d import cylinder
 
 
 class SpiderBot(box.Box):
+    leg_width = 3
+    leg_radius = 2
+    leg_circle_1_lenght = 4
+
     def __init__(self, length=10, width=10, height=3):
         box.Box.__init__(self, length=length, width=width, height=height)
         self.center = bone.Bone(parent=self)
-        # self.left_front = box.Box(parent=self, pos=)
+        self.leg_left_front_0 = cylinder.Cylinder(
+            parent=self,
+            radius=self.leg_radius,
+            length=self.leg_circle_1_lenght,
+            axis=(0, 1, 0),
+            up=(-1, 0, 0),
+            pos=(
+                length / 2.0 - self.leg_width / 2.0,
+                -self.leg_circle_1_lenght / 2.0,
+                -width / 2.0))
+
+        # self.leg_left_front_1 = box.Box(
+        #     parent=self.leg_left_front_0,
+        #     pos=(0, 0, 0),
+        #     lenght=self.leg_width,
+        #     height=self.leg_width,
+        #     width=20)
 
 
 class MyScene(scene.Scene):
