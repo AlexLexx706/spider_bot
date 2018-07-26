7920671cadc996ccb7fc5e2c010b56aed08d0970
alexlexx1 <alexlexx1@gmail.com>
2018-07-26 23:53:51 +0300

    add rotate step

--- spider_bot/__init__.py
+++ spider_bot/__init__.py
@@ -4,6 +4,8 @@ from engine_3d import box
 from engine_3d import sphere
 from spider_bot import leg
 from engine_3d import vector
+from engine_3d import transformations
+import numpy as np
 
 
 class SpiderBot(box.Box):
@@ -57,9 +59,13 @@ class SpiderBot(box.Box):
         self.half_step_len = 8
         self.step_height = 3
 
+        self.rotate_state = 0
+        self.turn_angle = 20.0 / 180.0 * math.pi
+
     def update(self):
         super(SpiderBot, self).update()
-        self.move_forward()
+        # self.move_forward()
+        self.rotate_step()
 
     def move_forward(self):
         if self.begin_move:
@@ -242,25 +248,119 @@ class SpiderBot(box.Box):
                 self.begin_move = True
                 self.move_state = 0
 
-        def rotate(self):
-            if self.begin_move:
-                self.start_time = time.time()
-                self.begin_move = False
-            dt = (time.time() - self.start_time) / self.move_time
+    def rotate_step(self):
+        if self.begin_move:
+            self.start_time = time.time()
+            self.begin_move = False
 
-            # move front left leg
+            # init state
             if self.rotate_state == 0:
-                pass
-            # move rear left leg
-            elif self.rotate_state == 1:
-                pass
-            # rotate all points
-            elif self.rotate_state == 2:
-                pass
-            # move rear right leg
-            elif self.rotate_state == 3:
-                pass
-            # move front right leg
-            elif self.rotate_state == 4:
-                pass
+                self.front_left_leg.move_end(
+                    self.front_left_pos)
+
+                self.front_right_leg.move_end(
+                    self.front_right_pos)
+
+                self.rear_left_leg.move_end(
+                    self.rear_left_pos)
+
+                self.rear_right_leg.move_end(
+                    self.rear_right_pos)
+
+        dt = (time.time() - self.start_time) / self.move_time
+        up_vector = vector.Vector(
+            0,
+            self.step_height * math.sin(math.pi * dt),
+            0)
+
+        # move front left leg
+        if self.rotate_state == 0:
+            if dt < 1.0:
+                angle = dt * self.turn_angle
+            else:
+                angle = self.turn_angle
+                self.begin_move = True
+                self.rotate_state = 1
+            self.front_left_leg.move_end(
+                up_vector + np.dot(
+                    self.front_left_pos,
+                    transformations.rotation_matrix(
+                        angle,
+                        vector.Vector(0, 1, 0))[:3, :3].T))
+        # move rear left leg
+        elif self.rotate_state == 1:
+            if dt < 1.0:
+                angle = dt * self.turn_angle
+            else:
+                angle = self.turn_angle
+                self.begin_move = True
+                self.rotate_state = 2
+
+            self.rear_left_leg.move_end(
+                up_vector + np.dot(
+                    self.rear_left_pos,
+                    transformations.rotation_matrix(
+                        angle,
+                        vector.Vector(0, 1, 0))[:3, :3].T))
+        # rotate all points
+        elif self.rotate_state == 2:
+            if dt < 1.0:
+                angle_1 = (1.0 - dt) * self.turn_angle
+                angle_2 = dt * -self.turn_angle
+            else:
+                angle_1 = 0
+                angle_2 = -self.turn_angle
+                self.begin_move = True
+                self.rotate_state = 3
+
+            mat_1 = transformations.rotation_matrix(
+                angle_1,
+                vector.Vector(0, 1, 0))[:3, :3].T
+
+            mat_2 = transformations.rotation_matrix(
+                angle_2,
+                vector.Vector(0, 1, 0))[:3, :3].T
+
+            self.front_left_leg.move_end(
+                np.dot(self.front_left_pos, mat_1))
+
+            self.rear_left_leg.move_end(
+                np.dot(self.rear_left_pos, mat_1))
+
+            self.front_right_leg.move_end(
+                np.dot(self.front_right_pos, mat_2))
+
+            self.rear_right_leg.move_end(
+                np.dot(self.rear_right_pos, mat_2))
+
+        # move rear right leg
+        elif self.rotate_state == 3:
+            if dt < 1.0:
+                angle = (1.0 - dt) * -self.turn_angle
+            else:
+                angle = 0
+                self.begin_move = True
+                self.rotate_state = 4
+
+            self.rear_right_leg.move_end(
+                up_vector + np.dot(
+                    self.rear_right_pos,
+                    transformations.rotation_matrix(
+                        angle,
+                        vector.Vector(0, 1, 0))[:3, :3].T))
+        # move front right leg
+        elif self.rotate_state == 4:
+            if dt < 1.0:
+                angle = (1.0 - dt) * -self.turn_angle
+            else:
+                angle = 0
+                self.begin_move = True
+                self.rotate_state = 0
+
+            self.front_right_leg.move_end(
+                up_vector + np.dot(
+                    self.front_right_pos,
+                    transformations.rotation_matrix(
+                        angle,
+                        vector.Vector(0, 1, 0))[:3, :3].T))
 
