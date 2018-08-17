98c2179a4645aa0e88974aebcbc53ab8eee7e933
alex <akalmykov@topcon.com>
2018-08-17 19:41:12 +0300

    add udp server

--- spider_bot/process_model.py
+++ spider_bot/process_model.py
@@ -2,6 +2,22 @@ import time
 from spider_bot import settings
 from spider_bot import fake_scene
 import spider_bot
+import socket
+
+
+def udp_server(host='127.0.0.1', port=1234):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    print("Listening on udp %s:%s" % (host, port))
+    s.bind((host, port))
+    while True:
+        (data, addr) = s.recvfrom(128 * 1024)
+        yield data
+
+
+for data in udp_server():
+    print("%r" % (data,))
 
 
 def main():
@@ -20,6 +36,7 @@ def main():
         if dt >= period:
             start_time = cur_time
             scene.update()
+            print(1)
         # make other actions
         else:
             time.sleep(sleep_period)
