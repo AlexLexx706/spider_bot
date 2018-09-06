55c232136cdb41004847db49bed93534501efe11
alex <akalmykov@topcon.com>
2018-09-06 16:52:43 +0300

    add not working yet

--- spider_bot/gui/scene.py
+++ spider_bot/gui/scene.py
@@ -1,7 +1,7 @@
 from engine_3d import scene
 import spider_bot
 from spider_bot.gui import client
-
+import numpy as np
 
 class Scene(scene.Scene):
     def __init__(self):
@@ -15,29 +15,17 @@ class Scene(scene.Scene):
         self.client.add_notify()
 
     def notify_handler(self, data):
-        mat = data['mat']
-        self.bot._matrix = mat[0]
+        data.body_mat
+        data.front_right_leg
+        data.front_left_leg
+        data.rear_right_leg
+        data.rear_left_leg
 
-        self.bot.front_right_leg._matrix =     mat[1][0]
-        self.bot.front_right_leg.p_0._matrix = mat[1][1]
-        self.bot.front_right_leg.p_1._matrix = mat[1][2]
-        self.bot.front_right_leg.p_2._matrix = mat[1][3]
-        self.bot.front_right_leg.end._matrix = mat[1][3]
+        mat = np.array(data.body_mat)
+        mat.shape = (4, 4)
 
-        self.bot.front_left_leg._matrix =     mat[2][0]
-        self.bot.front_left_leg.p_0._matrix = mat[2][1]
-        self.bot.front_left_leg.p_1._matrix = mat[2][2]
-        self.bot.front_left_leg.p_2._matrix = mat[2][3]
-        self.bot.front_left_leg.end._matrix = mat[2][3]
-
-        self.bot.rear_right_leg._matrix =     mat[3][0]
-        self.bot.rear_right_leg.p_0._matrix = mat[3][1]
-        self.bot.rear_right_leg.p_1._matrix = mat[3][2]
-        self.bot.rear_right_leg.p_2._matrix = mat[3][3]
-        self.bot.rear_right_leg.end._matrix = mat[3][3]
-
-        self.bot.rear_left_leg._matrix =     mat[4][0]
-        self.bot.rear_left_leg.p_0._matrix = mat[4][1]
-        self.bot.rear_left_leg.p_1._matrix = mat[4][2]
-        self.bot.rear_left_leg.p_2._matrix = mat[4][3]
-        self.bot.rear_left_leg.end._matrix = mat[4][3]
+        self.bot._matrix = mat
+        self.bot.front_right_leg.move_end(list(data.front_right_leg.end))
+        self.bot.front_left_leg.move_end(list(data.front_left_leg.end))
+        self.bot.rear_right_leg.move_end(list(data.rear_right_leg.end))
+        self.bot.rear_left_leg.move_end(list(data.rear_left_leg.end))
