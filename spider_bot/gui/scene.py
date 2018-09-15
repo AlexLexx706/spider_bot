3a73b87edb92240768eff499e1c9b0ef2e257e95
alexlexx1 <alexlexx1@gmail.com>
2018-09-15 17:15:31 +0300

    add

--- spider_bot/gui/scene.py
+++ spider_bot/gui/scene.py
@@ -19,19 +19,69 @@ class Scene(scene.Scene):
         mat = np.array(data.body_mat)
         mat.shape = (4, 4)
 
+        # ('shoulder_offset', ctypes.c_float),
+        # ('shoulder_lenght', ctypes.c_float),
+        # ('forearm_lenght', ctypes.c_float),
+
         self.bot._matrix = mat
         self.bot.front_right_leg.p_0.ang_y = data.front_right_leg.a_0
         self.bot.front_right_leg.p_1.ang_x = data.front_right_leg.a_1
         self.bot.front_right_leg.p_2.ang_x = data.front_right_leg.a_2
+        self.bot.front_right_leg.pos = data.front_right_leg.pos
+
+        pos = self.bot.front_right_leg.p_1.pos
+        pos[2] = data.front_right_leg.shoulder_offset
+        self.bot.front_right_leg.p_1.pos = pos
+        pos = self.bot.front_right_leg.p_2.pos
+        pos[2] = data.front_right_leg.shoulder_lenght
+        self.bot.front_right_leg.p_2.pos = pos
+        pos = self.bot.front_right_leg.end.pos
+        pos[2] = data.front_right_leg.shoulder_lenght
+        self.bot.front_right_leg.end.pos = pos
 
         self.bot.front_left_leg.p_0.ang_y = data.front_left_leg.a_0
         self.bot.front_left_leg.p_1.ang_x = data.front_left_leg.a_1
         self.bot.front_left_leg.p_2.ang_x = data.front_left_leg.a_2
+        self.bot.front_left_leg.pos = data.front_left_leg.pos
+
+        pos = self.bot.front_left_leg.p_1.pos
+        pos[2] = data.front_left_leg.shoulder_offset
+        self.bot.front_left_leg.p_1.pos = pos
+        pos = self.bot.front_left_leg.p_2.pos
+        pos[2] = data.front_left_leg.shoulder_lenght
+        self.bot.front_left_leg.p_2.pos = pos
+        pos = self.bot.front_left_leg.end.pos
+        pos[2] = data.front_left_leg.shoulder_lenght
+        self.bot.front_left_leg.end.pos = pos
 
         self.bot.rear_right_leg.p_0.ang_y = data.rear_right_leg.a_0
         self.bot.rear_right_leg.p_1.ang_x = data.rear_right_leg.a_1
         self.bot.rear_right_leg.p_2.ang_x = data.rear_right_leg.a_2
+        self.bot.rear_right_leg.pos = data.rear_right_leg.pos
+
+        pos = self.bot.rear_right_leg.p_1.pos
+        pos[2] = data.rear_right_leg.shoulder_offset
+        self.bot.rear_right_leg.p_1.pos = pos
+        pos = self.bot.rear_right_leg.p_2.pos
+        pos[2] = data.rear_right_leg.shoulder_lenght
+        self.bot.rear_right_leg.p_2.pos = pos
+        pos = self.bot.rear_right_leg.end.pos
+        pos[2] = data.rear_right_leg.shoulder_lenght
+        self.bot.rear_right_leg.end.pos = pos
 
         self.bot.rear_left_leg.p_0.ang_y = data.rear_left_leg.a_0
         self.bot.rear_left_leg.p_1.ang_x = data.rear_left_leg.a_1
         self.bot.rear_left_leg.p_2.ang_x = data.rear_left_leg.a_2
+        self.bot.rear_left_leg.pos = data.rear_left_leg.pos
+
+        pos = self.bot.rear_left_leg.p_1.pos
+        pos[2] = data.rear_left_leg.shoulder_offset
+        self.bot.rear_left_leg.p_1.pos = pos
+
+        pos = self.bot.rear_left_leg.p_2.pos
+        pos[2] = data.rear_left_leg.shoulder_lenght
+        self.bot.rear_left_leg.p_2.pos = pos
+
+        pos = self.bot.rear_left_leg.end.pos
+        pos[2] = data.rear_left_leg.shoulder_lenght
+        self.bot.rear_left_leg.end.pos = pos
